5cc398ca566df4759560ee9bf03d916a15e9fa3a
Mateus Costa <mateuscomp@gmail.com>
2018-11-25 23:18:50 +0100

    apply clippy suggestions

--- src/user/mod.rs
+++ src/user/mod.rs
@@ -4,7 +4,6 @@ mod artist;
 use user::authentication::get_spotify;
 use user::artist::{ Album, Artist };
 use rand::thread_rng;
-use rand::Rng;
 use rand::seq::SliceRandom;
 use rspotify::spotify::client::Spotify;
 use rspotify::spotify::senum::AlbumType;
@@ -15,12 +14,12 @@ pub fn get_followed_artists() {
     let artists_and_ids = get_all_followed_artists(&spotify);
     let artists_names = artists_and_ids
         .keys()
-        .map(|v| v.clone())
+        .cloned()
         .collect::<Vec<String>>();
 
     let randomized_artist_name = artists_names.choose(& mut thread_rng()).unwrap();
-    let artist_id = artists_and_ids.get(randomized_artist_name).unwrap();
-    let randomized_album = get_randomized_album(&spotify, artist_id);
+    let artist_id = &artists_and_ids[randomized_artist_name];
+    let randomized_album = get_randomized_album(&spotify, &artist_id);
 
     println!("You are going to listen to {} from {}", randomized_album.name, randomized_artist_name);
     println!("Here's the url: {}", randomized_album.url);
@@ -57,13 +56,13 @@ fn get_randomized_album(spotify: &Spotify, id: &str) -> Album {
     let randomized_album_from_response = albums.choose(& mut thread_rng()).unwrap();
     let name = &randomized_album_from_response.name;
     let id = &randomized_album_from_response.id;
-    let url = &randomized_album_from_response.external_urls.get("spotify").unwrap();
+    let url = &randomized_album_from_response.external_urls["spotify"];
 
-    return Album {
+    Album {
         name: name.to_string(),
         id: id.to_string(),
         url: url.to_string(),
-    };
+    }
 }
 
 #[cfg(test)]
