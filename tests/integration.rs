3582cacbecdd3f4bd18b02ac002cb7e2746c874b
Mateus Costa <mateuscomp@gmail.com>
2019-01-08 19:29:49 -0300

    apply fmt

--- tests/integration.rs
+++ tests/integration.rs
@@ -7,8 +7,10 @@ mod integration {
     #[test]
     fn calling_rget_without_args() {
         assert_cli::Assert::main_binary()
-            .stdout().contains("You are going to listen to")
-            .stdout().contains("Here's the url:")
+            .stdout()
+            .contains("You are going to listen to")
+            .stdout()
+            .contains("Here's the url:")
             .unwrap();
     }
 }
