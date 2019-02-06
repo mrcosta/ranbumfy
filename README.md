d51510821353f9eb951f8456f033aeb3d87d018b
Mateus Costa <mateuscomp@gmail.com>
2019-02-06 20:10:03 +0100

    randomize 7 albums and add future refactoring note

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 [![pipeline status](https://gitlab.com/mateuscosta/ranbumfy/badges/master/pipeline.svg)](https://gitlab.com/mateuscosta/ranbumfy/commits/master)
 
 randomize an album to be listen based in your followed artists
+
+# TODOs
+
+* put music_client in a service, since doesnt make sense to entities to have this kind of dependency
+that in the end is going to call external services
--- src/user.rs
+++ src/user.rs
@@ -9,29 +9,23 @@ use rand::Rng;
 
 pub fn draw_an_album_to_list(music_client: &MusicClient) {
     let artists = followed_artists(music_client);
-    let randomized_artist = randomize_artist(artists);
-    let randomized_album = randomized_artist.draw_an_album(music_client);
-    // TODO: create profile object and pass music client for it
-    // profile.followed_artists() should be the api
-    // artist_service that receive the music_client instead of the entity artist itself
-    // it makes more sense, since its can be tricky to main an entity with this kind of dependency
 
-    println!(
-        "Listen to {} from {}: {}",
-        randomized_album.name, randomized_artist.name, randomized_album.url
-    );
+    for _ in 0..7 {
+        let randomized_artist = randomize_artist(&artists);
+        let randomized_album = randomized_artist.draw_an_album(music_client);
+        println!(
+            "Listen to {} from {}: {}",
+            randomized_album.name, randomized_artist.name, randomized_album.url
+        );
+    }
 }
 
-fn randomize_artist(mut artists: Vec<Artist>) -> Artist {
+fn randomize_artist(artists: &Vec<Artist>) -> Artist {
     let randomized_artist_index = thread_rng().gen_range(0, artists.len());
-    artists.swap_remove(randomized_artist_index)
-}
+    let artist = artists.get(randomized_artist_index).unwrap();
 
-#[cfg(test)]
-mod tests {
-
-    #[test]
-    fn test_zero() {
-        assert_eq!("zero", "zero");
+    Artist {
+        id: artist.id.to_owned(),
+        name: artist.name.to_owned(),
     }
 }
