c92d8bbacbebba7c62529f73930f0dc05b700fcf
Mateus Costa <mateuscomp@gmail.com>
2019-03-08 20:22:14 +0100

    use clap to get options from command line

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,24 @@ use env_logger;
 use spotify_music_service::authentication::get_spotify_client;
 use spotify_music_service::SpotifyClient;
 use user::UserService;
+use clap::App;
 
 fn main() {
     // TODO: draw a recent followed artist to listen to
     // get followed artists that release album in 2019 and it's events
     // implement display for artists
-    // create crate
+
+    let app = App::new("ranbumfy")
+        .version("0.1")
+        .author("Mateus Costa <mateuscomp@gmail.com>")
+        .args_from_usage("-w, --with-albums 'It draws also album with the artist'");
+
+    let matches = app.get_matches();
+
     let user_service = init();
-    user_service.draw_albums_to_list();
+    if matches.is_present("with-albums") {
+        user_service.draw_albums_to_list();
+    }
 }
 
 fn init() -> UserService {
